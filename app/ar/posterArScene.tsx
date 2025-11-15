f8302f7b122f9fedf2ca875e8db24b2616b108f8
Trik-mink <tristan2810ly@gmail.com>
2025-11-14 22:42:18 -0500

    delete_chatbot

--- app/ar/posterArScene.tsx
+++ app/ar/posterArScene.tsx
@@ -6,11 +6,8 @@ import {
     ViroARScene,
     ViroARTrackingTargets,
     ViroAmbientLight,
-    ViroBox,
     ViroDirectionalLight,
-    ViroMaterials,
     ViroNode,
-    ViroText,
 } from "@reactvision/react-viro";
 import React from "react";
 import { POSTER_TARGET_IDS } from "./posterTargets";
@@ -46,16 +43,6 @@ try {
   console.log("[PosterArScene] Tracking targets already created");
 }
 
-try {
-  ViroMaterials.createMaterials({
-    chatBubbleBg: {
-      diffuseColor: "#ffffff", // white chat box
-    },
-  });
-} catch (e) {
-  console.log("[PosterArScene] Materials already created");
-}
-
 // -----------------------------
 // Model configurations
 // -----------------------------
@@ -63,25 +50,21 @@ try {
 const POSTER_CONFIGS = {
   [POSTER_TARGET_IDS.FLOWER]: {
     model: require("../../assets/models/flower.glb"),
-    message: "You found a beautiful flower!",
     scale: [0.05, 0.05, 0.05] as [number, number, number],
     rotation: [0, 180, 0] as [number, number, number],
   },
   [POSTER_TARGET_IDS.GHOST]: {
     model: require("../../assets/models/ghost_ur.glb"),
-    message: "A friendly ghost appears!",
     scale: [0.05, 0.05, 0.05] as [number, number, number],
     rotation: [0, 0, 0] as [number, number, number],
   },
   [POSTER_TARGET_IDS.ROCKY]: {
     model: require("../../assets/models/Rocky3.glb"),
-    message: "It's Rocky, the Rochester mascot!",
     scale: [0.05, 0.05, 0.05] as [number, number, number],
     rotation: [0, 180, 0] as [number, number, number],
   },
   [POSTER_TARGET_IDS.TEACHER]: {
     model: require("../../assets/models/teacher.glb"),
-    message: "A wise teacher appears!",
     scale: [0.6, 0.6, 0.6] as [number, number, number],
     rotation: [0, 0, 0] as [number, number, number],
   },
@@ -105,47 +88,15 @@ const PosterArScene: React.FC = () => {
         target={targetId}
         onAnchorFound={() => handleAnchorFound(targetId)}
       >
-        {/* Anchor at poster center; put character + chat just above */}
+        {/* 3D Model positioned above the poster */}
         <ViroNode position={[0, 0.08, 0]}>
-          {/* 3D Model on the left */}
-          <ViroNode
-            position={[-0.16, 0.04, 0]}
-            transformBehaviors={["billboardY"]}
-          >
+          <ViroNode transformBehaviors={["billboardY"]}>
             <Viro3DObject
               source={config.model}
               type="GLB"
               scale={config.scale}
               rotation={config.rotation}
               position={[0, 0, 0]}
-            />
-          </ViroNode>
-
-          {/* Chat box to the right */}
-          <ViroNode
-            position={[0.20, 0.06, 0]}
-            transformBehaviors={["billboardY"]}
-            scale={[0.9, 0.9, 0.9]}
-          >
-            {/* White 3D chat box */}
-            <ViroBox
-              width={0.24}
-              height={0.08}
-              length={0.02}
-              materials={["chatBubbleBg"]}
-            />
-
-            {/* Text: centered and slightly in front of box face */}
-            <ViroText
-              text={config.message}
-              width={0.20}
-              height={0.06}
-              position={[0, 0, 0.011]}
-              style={{
-                fontSize: 12,
-                color: "#111827",
-                textAlign: "center",
-              }}
             />
           </ViroNode>
         </ViroNode>
